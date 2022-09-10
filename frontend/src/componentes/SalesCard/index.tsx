3e966ddc2e14a6f70115676ec67f7d5542ec175e
Vinicius Gonçalves <viniciiussilva97@outlook.com>
2022-09-10 11:42:51 -0300

    Security config

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -74,7 +74,7 @@ function SalesCard() {
                         <tr>
                             <td className="show992">#341</td>
                             <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
+                            <td>Nat.celhas</td>
                             <td className="show992">15</td>
                             <td className="show992">11</td>
                             <td>R$ 55300.00</td>
